8b24611364bdbbe9027069c7c39d550e4b0de6af
Michael Drogalis <mjd3089@rit.edu>
2017-05-17 18:38:48 -0700

    Initial commit.

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject pyroclast-clojure "0.1.0-SNAPSHOT"
+  :description "A Clojure client for Pyroclast"
+  :url "https://github.com/onyx-platform/pyroclast-clojure"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [clj-http "3.5.0"]])
