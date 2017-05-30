8a284521dac71db0689079ca58c361484c5c104d
Michael Drogalis <mjd3089@rit.edu>
2017-05-29 21:26:16 -0700

    Release 0.1.3.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.pyroclast/pyroclast-clojure "0.1.3-SNAPSHOT"
+(defproject io.pyroclast/pyroclast-clojure "0.1.3"
   :description "A Clojure client for Pyroclast"
   :url "https://github.com/onyx-platform/pyroclast-clojure"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
