d9ffa8b9f1c072ffe50d9ddafe5331de8dc4c497
Michael Drogalis <mjd3089@rit.edu>
2017-05-29 21:27:13 -0700

    Back to snapshot.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject io.pyroclast/pyroclast-clojure "0.1.3"
+(defproject io.pyroclast/pyroclast-clojure "0.1.4-SNAPSHOT"
   :description "A Clojure client for Pyroclast"
   :url "https://github.com/onyx-platform/pyroclast-clojure"
   :dependencies [[org.clojure/clojure "1.9.0-alpha17"]
