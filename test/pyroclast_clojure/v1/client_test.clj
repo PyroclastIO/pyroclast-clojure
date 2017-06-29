20df6f711c8d5430625af69ec36c073e9d70c875
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-06-29 11:15:49 -0700

    Bench inputs.

--- test/pyroclast_clojure/v1/client_test.clj
+++ test/pyroclast_clojure/v1/client_test.clj
@@ -3,11 +3,33 @@
             [clojure.test :refer [is deftest]]))
 
 (def config
-  {:read-api-key "26ee27ed-8d6c-481f-894c-6e2b219c2865"
-   :write-api-key "0e0efdd3-293e-4ef7-995d-543d17c65d26"
-   :topic-id "topic-a0e4227b-6536-44e5-b68d-ac5a054ce2e7"
+  {:read-api-key "8e0dfbee-eede-4408-b0d2-ba4bde27c9f6" 
+   :write-api-key "38afdad0-0598-415b-8a08-9b1b5efc0a3e"
+   :topic-id "topic-6856aae4-ba19-4d57-9949-5d35a5d5caba"
    :endpoint "http://localhost:10556"
    :format :json})
+
+(defn bench [] 
+  (time 
+   (let [batch (mapv (fn [v]
+                       {:value {:my :value}})
+                     (range 100))] 
+     (dotimes [n-iters 100]
+       (let [n-requests 100
+             n-threads 1
+             n-sent (atom 0)] 
+         (pmap (fn [r] 
+                 (dotimes [i (int (/ n-requests n-threads))]
+                   (client/send-events-async!
+                    config 
+                    (fn [result] 
+                      (swap! n-sent inc))
+                    batch)))
+               (range n-threads))
+         (loop []
+           (when (not= @n-sent n-requests)
+             (Thread/sleep 100)
+             (recur))))))))
 
 (deftest producer-tests
   (is (= {:created true} (client/send-event! config {:value {:event-type "page-visit" :page "/home" :timestamp 1495072835000}})))
