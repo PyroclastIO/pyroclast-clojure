cc1042fa2d66c0623f8acad8d650a89081f42e09
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-10-19 17:28:59 -0700

    Better error response handling / messages.

--- test/pyroclast_clojure/v1/topic_test.clj
+++ test/pyroclast_clojure/v1/topic_test.clj
@@ -3,35 +3,35 @@
             [pyroclast-clojure.util :as u]
             [clojure.test :refer [is deftest]]))
 
-(deftest ^:topic producer-tests
-  (let [config (:client-config (u/load-config "config.edn"))]
-    (is @(client/topic-send-event! config {:value {:event-type "page-visit" :page "/home" :timestamp 1495072835000}}))
-
-    (is @(client/topic-send-events! config [{:value {:event-type "page-visit" :page "/home" :timestamp 1495072835000}}
-                                            {:value {:event-type "page-visit" :page "/console" :timestamp 1495072895032}}]))
-
+(deftest ^:topic topic-tests
+  (let [config (:topic-client-config (u/load-config "config.edn"))]
     (let [group "my-subscriber-group"
-          consumer-instance-map @(client/topic-subscribe config group)]
+         consumer-instance-map (client/topic-subscribe config group)]
+      (client/topic-consumer-seek-end config consumer-instance-map)
+      (is @(client/topic-send-event! config {:value {:event-type "page-visit" :page "/home" :timestamp 1495072835000}}))
+      (is @(client/topic-send-events! config [{:value {:event-type "page-visit" :page "/home" :timestamp 1495072835000}}
+                                              {:value {:event-type "page-visit" :page "/console" :timestamp 1495072895032}}]))
       (is (pos? (count @(client/topic-consumer-poll! config consumer-instance-map))))
       (is (client/topic-consumer-commit-offsets config consumer-instance-map))
       (is (empty? @(client/topic-consumer-poll! config consumer-instance-map))))))
 
-(deftest ^:performance producer-tests
-  (is true)
-  (time
-   (let [config (:client-config (u/load-config "config.edn"))]
-     (dotimes [r 50]
-       (run! deref (mapv (fn [i]
-                           (client/topic-send-events!
-                            config
-                            [{:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
-                             {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}]))
-                         (range 50)))))))
+;; Disable perf test for now as it messes with topic test's commit history
+; (deftest ^:performance prod-perf-tests
+;   (is true)
+;   (time
+;    (let [config (:topic-client-config (u/load-config "config.edn"))]
+;      (dotimes [r 50]
+;        (run! deref (mapv (fn [i]
+;                            (client/topic-send-events!
+;                             config
+;                             [{:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}
+;                              {:value {:event-type "page-visit" :page "store" :timestamp 1495072835000}}]))
+;                          (range 50)))))))
